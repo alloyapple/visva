055942f92934339653cc318e8e0ec7cfbe0d3f5d
tranduc.bk.1905 <tranduc.bk.1905@gmail.com>
2013-01-15 03:33:40 +0000

    Init Learn app project.

---
+++ IOS/LearnApp/LearnApp/TDAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TDAppDelegate.h
+//  LearnApp
+//
+//  Created by tranduc on 1/15/13.
+//  Copyright (c) 2013 tranduc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TDAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
