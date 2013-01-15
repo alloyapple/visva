90ddde724f4f723b34494bc8ad8796596ca8a184
tranduc.bk.1905@gmail.com <tranduc.bk.1905@gmail.com@93bdda27-e0ef-8ee2-a4e7-4542826b0fda>
2013-01-15 03:27:13 +0000

    Add

---
+++ trunk/IOS/IDManager/IDManager/TDAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  TDAppDelegate.h
+//  IDManager
+//
+//  Created by tranduc on 1/15/13.
+//  Copyright (c) 2013 tranduc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class TDViewController;
+
+@interface TDAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) TDViewController *viewController;
+
+@end
