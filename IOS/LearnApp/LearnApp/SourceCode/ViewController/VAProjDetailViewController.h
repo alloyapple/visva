8f79da3de50c7e8dd8609136583636fa008485e5
tranduc.bk.1905 <tranduc.bk.1905@gmail.com>
2013-01-17 14:50:09 +0000

    Add VAProjDetailViewController

---
+++ IOS/LearnApp/LearnApp/SourceCode/ViewController/VAProjDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  VAProjDetailViewController.h
+//  LearnApp
+//
+//  Created by tranduc on 1/17/13.
+//  Copyright (c) 2013 tranduc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VAProjDetailViewController : UIViewController
+
+@end
