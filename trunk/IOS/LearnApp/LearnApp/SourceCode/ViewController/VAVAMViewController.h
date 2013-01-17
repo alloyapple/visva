88962dc24d2bece636cb0fb01e1bbf4190bf6767
tranduc.bk.1905@gmail.com <tranduc.bk.1905@gmail.com@93bdda27-e0ef-8ee2-a4e7-4542826b0fda>
2013-01-17 14:22:20 +0000

    Update storyboard. Create view controllers.

---
+++ trunk/IOS/LearnApp/LearnApp/SourceCode/ViewController/VAVAMViewController.h
@@ -0,0 +1,13 @@
+//
+//  VAVAMViewController.h
+//  LearnApp
+//
+//  Created by tranduc on 1/17/13.
+//  Copyright (c) 2013 tranduc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VAVAMViewController : UIViewController
+
+@end
