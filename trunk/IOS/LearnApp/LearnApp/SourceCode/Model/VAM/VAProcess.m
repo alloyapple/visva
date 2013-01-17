64f75e3a6b0c0ef3f4b1dbff1ba4786ae8cda61e
tamnguyenthe91@gmail.com <tamnguyenthe91@gmail.com@93bdda27-e0ef-8ee2-a4e7-4542826b0fda>
2013-01-17 18:49:22 +0000

    add VAProcess class

---
+++ trunk/IOS/LearnApp/LearnApp/SourceCode/Model/VAM/VAProcess.m
@@ -0,0 +1,13 @@
+//
+//  VAProcess.m
+//  LearnApp
+//
+//  Created by Tam Nguyen on 1/18/13.
+//  Copyright (c) 2013 tranduc. All rights reserved.
+//
+
+#import "VAProcess.h"
+
+@implementation VAProcess
+
+@end
