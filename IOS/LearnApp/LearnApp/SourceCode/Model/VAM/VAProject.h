5f8c41faf1944a5bed2838ff6b25a15276f072e9
tranduc.bk.1905 <tranduc.bk.1905@gmail.com>
2013-01-17 14:35:47 +0000

    VAProject

---
+++ IOS/LearnApp/LearnApp/SourceCode/Model/VAM/VAProject.h
@@ -0,0 +1,13 @@
+//
+//  VAProject.h
+//  LearnApp
+//
+//  Created by tranduc on 1/17/13.
+//  Copyright (c) 2013 tranduc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface VAProject : NSObject
+
+@end
