5592b85f25ab9d3e0fa37bbcc98b1e0b06e965aa
tamnguyenthe91@gmail.com <tamnguyenthe91@gmail.com@93bdda27-e0ef-8ee2-a4e7-4542826b0fda>
2013-01-17 18:25:40 +0000

    add VAStep

---
+++ trunk/IOS/LearnApp/LearnApp/SourceCode/Model/VAM/VAStep.h
@@ -0,0 +1,25 @@
+//
+//  VAStep.h
+//  LearnApp
+//
+//  Created by Tam Nguyen on 1/17/13.
+//  Copyright (c) 2013 tranduc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@class VAProcess;
+@interface VAStep : NSObject
+
+@property (retain, nonatomic) NSString *Project_name;
+@property (retain, nonatomic) NSString *Process_name;
+@property (retain, nonatomic) NSNumber *Step_id;
+@property (retain, nonatomic) NSNumber *Step_no;
+@property (retain, nonatomic) NSString *Step_description;
+@property (retain, nonatomic) NSNumber *Version_id;
+@property (retain, nonatomic) NSNumber *Previous_vers_id;
+@property (retain, nonatomic) NSString *Video_file_name;
+
+//additional properties for OOP
+@property (assign, nonatomic) VAProcess *parentProcess;
+@end
