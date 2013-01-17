553c1c661d394e3605d893ea23afeb586b9ba169
tamnguyenthe91 <tamnguyenthe91@gmail.com>
2013-01-17 18:25:40 +0000

    add VAStep

---
+++ IOS/LearnApp/LearnApp/SourceCode/Model/VAM/VAStep.m
@@ -0,0 +1,43 @@
+//
+//  VAStep.m
+//  LearnApp
+//
+//  Created by Tam Nguyen on 1/17/13.
+//  Copyright (c) 2013 tranduc. All rights reserved.
+//
+
+#import "VAStep.h"
+
+@implementation VAStep
+
+@synthesize Project_name;
+@synthesize Process_name;
+@synthesize Step_id;
+@synthesize Step_no;
+@synthesize Step_description;
+@synthesize Version_id;
+@synthesize Previous_vers_id;
+@synthesize Video_file_name;
+
+@synthesize parentProcess;
+
+-(id)init {
+    self = [super init];
+    if (self) {
+        //
+    }
+    return self;
+}
+
+-(void)dealloc {
+    [self.Project_name release];
+    [self.Process_name release];
+    [self.Step_id release];
+    [self.Step_no release];
+    [self.Step_description release];
+    [self.Version_id release];
+    [self.Previous_vers_id release];
+    [self.Video_file_name release];
+    [super dealloc];
+}
+@end
