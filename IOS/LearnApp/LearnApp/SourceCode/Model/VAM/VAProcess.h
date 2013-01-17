51e77de0270d418e5a1c185ac5d8ce5d70fa2755
tamnguyenthe91 <tamnguyenthe91@gmail.com>
2013-01-17 18:49:22 +0000

    add VAProcess class

---
+++ IOS/LearnApp/LearnApp/SourceCode/Model/VAM/VAProcess.h
@@ -0,0 +1,13 @@
+//
+//  VAProcess.h
+//  LearnApp
+//
+//  Created by Tam Nguyen on 1/18/13.
+//  Copyright (c) 2013 tranduc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface VAProcess : NSObject
+
+@end
