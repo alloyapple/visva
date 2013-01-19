67f3ee3f6666d592e108d2b9f24a3348d7d3411f
kieuducthang <kieuducthang@gmail.com>
2013-01-19 14:40:29 +0000

    update fix bugs delete process

--- Android/LeanAppAndroid/src/vsvteam/outsource/leanappandroid/activity/circletiming/CircleTimeActivity.java
+++ Android/LeanAppAndroid/src/vsvteam/outsource/leanappandroid/activity/circletiming/CircleTimeActivity.java
@@ -5,7 +5,6 @@ import kankan.wheel.widget.adapters.ArrayWheelAdapter;
 import vsvteam.outsource.leanappandroid.R;
 import vsvteam.outsource.leanappandroid.activity.home.VSVTeamBaseActivity;
 import android.content.Context;
-import android.content.Intent;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.view.View;
