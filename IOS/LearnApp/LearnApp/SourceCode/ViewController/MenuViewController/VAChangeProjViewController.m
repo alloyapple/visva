79aaa9cb619b40066b0d383d982140a7818155a7
tranduc.bk.1905 <tranduc.bk.1905@gmail.com>
2013-01-19 05:31:10 +0000

    Add menu viewControlles.

---
+++ IOS/LearnApp/LearnApp/SourceCode/ViewController/MenuViewController/VAChangeProjViewController.m
@@ -0,0 +1,38 @@
+//
+//  VAChangeProjViewController.m
+//  LearnApp
+//
+//  Created by tranduc on 1/19/13.
+//  Copyright (c) 2013 tranduc. All rights reserved.
+//
+
+#import "VAChangeProjViewController.h"
+
+@interface VAChangeProjViewController ()
+
+@end
+
+@implementation VAChangeProjViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ IOS/LearnApp/LearnApp/SourceCode/ViewController/MenuViewController/VAVersionViewController.h
@@ -0,0 +1,13 @@
+//
+//  VAVersionViewController.h
+//  LearnApp
+//
+//  Created by tranduc on 1/19/13.
+//  Copyright (c) 2013 tranduc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VAVersionViewController : UIViewController
+
+@end
