da9eaf52ecc4f15208cf0ca47e08d5141392d7d2
tranduc.bk.1905@gmail.com <tranduc.bk.1905@gmail.com@93bdda27-e0ef-8ee2-a4e7-4542826b0fda>
2013-01-19 05:33:02 +0000

    Add TextFieldCell.

---
+++ trunk/IOS/LearnApp/LearnApp/SourceCode/Libs/ToolKit/TDTextFieldCell.h
@@ -0,0 +1,15 @@
+//
+//  TDTextFieldCell.h
+//  LearnApp
+//
+//  Created by tranduc on 1/19/13.
+//  Copyright (c) 2013 tranduc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TDTextFieldCell : UITableViewCell
+@property (retain, nonatomic) IBOutlet UILabel *lbTitle;
+@property (retain, nonatomic) IBOutlet UITextField *tfValue;
+
+@end
