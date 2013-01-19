3c9ea14bd841d2d0de74a48e1ffd24d591946478
tranduc.bk.1905@gmail.com <tranduc.bk.1905@gmail.com@93bdda27-e0ef-8ee2-a4e7-4542826b0fda>
2013-01-19 05:19:06 +0000

    Re struct folder.

---
+++ trunk/IOS/LearnApp/LearnApp/SourceCode/ViewController/MenuViewController/VABookViewController.m
@@ -0,0 +1,38 @@
+//
+//  VABookViewController.m
+//  LearnApp
+//
+//  Created by tranduc on 1/19/13.
+//  Copyright (c) 2013 tranduc. All rights reserved.
+//
+
+#import "VABookViewController.h"
+
+@interface VABookViewController ()
+
+@end
+
+@implementation VABookViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ trunk/IOS/LearnApp/LearnApp/SourceCode/ViewController/RooViewController/VARootViewController.m
@@ -0,0 +1,38 @@
+//
+//  VARootViewController.m
+//  LearnApp
+//
+//  Created by tranduc on 1/19/13.
+//  Copyright (c) 2013 tranduc. All rights reserved.
+//
+
+#import "VARootViewController.h"
+
+@interface VARootViewController ()
+
+@end
+
+@implementation VARootViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
