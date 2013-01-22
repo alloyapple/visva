b4a08ad3492d39bd3f2e6bbd4a0b378f5c18bdd5
tranduc.bk.1905@gmail.com <tranduc.bk.1905@gmail.com@93bdda27-e0ef-8ee2-a4e7-4542826b0fda>
2013-01-22 04:30:26 +0000

    Add

---
+++ trunk/IOS/IDManager/IDManager/Source/ViewController/LoginView/VALoginController.h
@@ -0,0 +1,13 @@
+//
+//  VALoginController.h
+//  IDManager
+//
+//  Created by tranduc on 1/22/13.
+//  Copyright (c) 2013 tranduc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VALoginController : UIViewController
+
+@end
---
+++ trunk/IOS/IDManager/IDManager/Source/ViewController/LoginView/VALoginController.m
@@ -0,0 +1,38 @@
+//
+//  VALoginController.m
+//  IDManager
+//
+//  Created by tranduc on 1/22/13.
+//  Copyright (c) 2013 tranduc. All rights reserved.
+//
+
+#import "VALoginController.h"
+
+@interface VALoginController ()
+
+@end
+
+@implementation VALoginController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
