cf2e29219dc36c247d3b5a2ac328f101c825b4bc
kieuducthang <kieuducthang@gmail.com>
2013-02-24 23:18:53 +0000

    add new source for dropbox + gg drive

---
+++ Android/IDManager/src/visvateam/outsource/idmanager/activities/synccloud/DropBoxSyncActivity.java
@@ -0,0 +1,322 @@
+package visvateam.outsource.idmanager.activities.synccloud;
+
+import java.io.File;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.Toast;
+import com.dropbox.client2.DropboxAPI;
+import com.dropbox.client2.android.AndroidAuthSession;
+import com.dropbox.client2.android.AuthActivity;
+import com.dropbox.client2.session.AccessTokenPair;
+import com.dropbox.client2.session.AppKeyPair;
+import com.dropbox.client2.session.Session.AccessType;
+import com.dropbox.client2.session.TokenPair;
+import visvateam.outsource.idmanager.activities.R;
+import visvateam.outsource.idmanager.exportcontroller.dropbox.DropBoxController;
+import visvateam.outsource.idmanager.exportcontroller.excelcreator.ExcelDocumentController;
+
+public class DropBoxSyncActivity extends Activity {
+	private static final String TAG = "DBRoulette";
+
+	// /////////////////////////////////////////////////////////////////////////
+	// Your app-specific settings. //
+	// /////////////////////////////////////////////////////////////////////////
+
+	// Replace this with your app key and secret assigned by Dropbox.
+	// Note that this is a really insecure way to do this, and you shouldn't
+	// ship code which contains your key & secret in such an obvious way.
+	// Obfuscation is good.
+	final static private String APP_KEY = "fxh7pnxcqbg3qwy";
+	final static private String APP_SECRET = "fjk6z73ot28n1t3";
+
+	// If you'd like to change the access type to the full Dropbox instead of
+	// an app folder, change this value.
+	final static private AccessType ACCESS_TYPE = AccessType.APP_FOLDER;
+
+	// /////////////////////////////////////////////////////////////////////////
+	// End app-specific settings. //
+	// /////////////////////////////////////////////////////////////////////////
+
+	// You don't need to change these, leave them alone.
+	final static private String ACCOUNT_PREFS_NAME = "prefs";
+	final static private String ACCESS_KEY_NAME = "ACCESS_KEY";
+	final static private String ACCESS_SECRET_NAME = "ACCESS_SECRET";
+	final static private String MY_FILE_LOCATION = "/Documents/";
+
+	DropboxAPI<AndroidAuthSession> mApi; 
+
+	private boolean mLoggedIn;
+
+	// Android widgets
+	private Button btnLinkToDropbox;
+	private Button btnStartSync;
+	private String mCameraFileName;
+	private ExcelDocumentController exportExcel;
+	public static final String FILE_NAME_EXCEL = "test.xls";
+	private static final String FILE_NAME_PDF = "test";
+	public static final String PATH_FOLDER_DOCUMENT = Environment.getExternalStorageDirectory()
+			.getPath() + "/LeanApp/Documents/";
+
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		if (savedInstanceState != null) {
+			mCameraFileName = savedInstanceState.getString("mCameraFileName");
+		}
+
+		// We create a new AuthSession so that we can use the Dropbox API.
+		AndroidAuthSession session = buildSession();
+		mApi = new DropboxAPI<AndroidAuthSession>(session);
+
+		// Basic Android widgets
+		setContentView(R.layout.page_sync_dropbox);
+
+		checkAppKeySetup();
+
+		btnLinkToDropbox = (Button) findViewById(R.id.btn_link_to_dropbox);
+
+		btnLinkToDropbox.setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+				// This logs you out if you're logged in, or vice versa
+				if (mLoggedIn) {
+					logOut();
+				} else {
+					// Start the remote authentication
+					mApi.getSession().startAuthentication(DropBoxSyncActivity.this);
+				}
+			}
+		});
+
+		// This is the button to take a photo
+		btnStartSync = (Button) findViewById(R.id.btn_start_sync);
+
+		btnStartSync.setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+				if (!isExternalStorageAvailable()) {
+					showToast("Sdcard is unvailable.Check it again");
+				} else {
+					createExcelFile(FILE_NAME_EXCEL);
+				}
+			}
+		});
+
+		// Display the proper UI state if logged in or not
+		setLoggedIn(mApi.getSession().isLinked());
+
+	}
+
+	@Override
+	protected void onSaveInstanceState(Bundle outState) {
+		outState.putString("mCameraFileName", mCameraFileName);
+		super.onSaveInstanceState(outState);
+	}
+
+	
+
+	/**
+	 * create file excel in folder document and check sdcard
+	 */
+	private void createExcelFile(String fileExcelName) {
+
+		/* init export */
+		exportExcel = new ExcelDocumentController(DropBoxSyncActivity.this);
+
+		/* create file excel in sdcard */
+		exportExcel.saveExcelFile(fileExcelName);
+
+		/* export file excel to dropbox */
+		exportExcelToDropBox(fileExcelName);
+
+	}
+
+	/**
+	 * export excel file to dropbox
+	 * 
+	 * @param fileName
+	 */
+
+	private void exportExcelToDropBox(String fileName) {
+
+		/* create folder hold excel file */
+		File folderFile = new File(PATH_FOLDER_DOCUMENT);
+		if (!folderFile.exists()) {
+			folderFile.mkdirs();
+		}
+
+		/* upload file to dropbox */
+		File fileExcel = new File(PATH_FOLDER_DOCUMENT + fileName);
+		DropBoxController newFile = new DropBoxController(DropBoxSyncActivity.this, mApi, MY_FILE_LOCATION,
+				fileExcel);
+		newFile.execute();
+	}
+
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+		AndroidAuthSession session = mApi.getSession();
+
+		// The next part must be inserted in the onResume() method of the
+		// activity from which session.startAuthentication() was called, so
+		// that Dropbox authentication completes properly.
+		if (session.authenticationSuccessful()) {
+			try {
+				// Mandatory call to complete the auth
+				session.finishAuthentication();
+
+				// Store it locally in our app for later use
+				TokenPair tokens = session.getAccessTokenPair();
+				storeKeys(tokens.key, tokens.secret);
+				setLoggedIn(true);
+			} catch (IllegalStateException e) {
+				showToast("Couldn't authenticate with Dropbox:" + e.getLocalizedMessage());
+				Log.i(TAG, "Error authenticating", e);
+			}
+		}
+	}
+
+	private void logOut() {
+		// Remove credentials from the session
+		mApi.getSession().unlink();
+
+		// Clear our stored keys
+		clearKeys();
+		// Change UI state to display logged out version
+		setLoggedIn(false);
+	}
+
+	/**
+	 * Convenience function to change UI state based on being logged in
+	 */
+	private void setLoggedIn(boolean loggedIn) {
+		mLoggedIn = loggedIn;
+		if (loggedIn) {
+			btnLinkToDropbox.setText("Unlink from Dropbox");
+		} else {
+			btnLinkToDropbox.setText("Link with Dropbox");
+		}
+	}
+
+	private void checkAppKeySetup() {
+		// Check to make sure that we have a valid app key
+		if (APP_KEY.startsWith("CHANGE") || APP_SECRET.startsWith("CHANGE")) {
+			showToast("You must apply for an app key and secret from developers.dropbox.com, and add them to the DBRoulette ap before trying it.");
+			finish();
+			return;
+		}
+
+		// Check if the app has set up its manifest properly.
+		Intent testIntent = new Intent(Intent.ACTION_VIEW);
+		String scheme = "db-" + APP_KEY;
+		String uri = scheme + "://" + AuthActivity.AUTH_VERSION + "/test";
+		testIntent.setData(Uri.parse(uri));
+		PackageManager pm = getPackageManager();
+		if (0 == pm.queryIntentActivities(testIntent, 0).size()) {
+			showToast("URL scheme in your app's "
+					+ "manifest is not set up correctly. You should have a "
+					+ "com.dropbox.client2.android.AuthActivity with the " + "scheme: " + scheme);
+			finish();
+		}
+	}
+
+	private void showToast(String msg) {
+		Toast error = Toast.makeText(this, msg, Toast.LENGTH_LONG);
+		error.show();
+	}
+
+	/**
+	 * Shows keeping the access keys returned from Trusted Authenticator in a
+	 * local store, rather than storing user name & password, and
+	 * re-authenticating each time (which is not to be done, ever).
+	 * 
+	 * @return Array of [access_key, access_secret], or null if none stored
+	 */
+	private String[] getKeys() {
+		SharedPreferences prefs = getSharedPreferences(ACCOUNT_PREFS_NAME, 0);
+		String key = prefs.getString(ACCESS_KEY_NAME, null);
+		String secret = prefs.getString(ACCESS_SECRET_NAME, null);
+		if (key != null && secret != null) {
+			String[] ret = new String[2];
+			ret[0] = key;
+			ret[1] = secret;
+			return ret;
+		} else {
+			return null;
+		}
+	}
+
+	/**
+	 * Shows keeping the access keys returned from Trusted Authenticator in a
+	 * local store, rather than storing user name & password, and
+	 * re-authenticating each time (which is not to be done, ever).
+	 */
+	private void storeKeys(String key, String secret) {
+		// Save the access key for later
+		SharedPreferences prefs = getSharedPreferences(ACCOUNT_PREFS_NAME, 0);
+		Editor edit = prefs.edit();
+		edit.putString(ACCESS_KEY_NAME, key);
+		edit.putString(ACCESS_SECRET_NAME, secret);
+		edit.commit();
+	}
+
+	private void clearKeys() {
+		SharedPreferences prefs = getSharedPreferences(ACCOUNT_PREFS_NAME, 0);
+		Editor edit = prefs.edit();
+		edit.clear();
+		edit.commit();
+	}
+
+	private AndroidAuthSession buildSession() {
+		AppKeyPair appKeyPair = new AppKeyPair(APP_KEY, APP_SECRET);
+		AndroidAuthSession session;
+
+		String[] stored = getKeys();
+		if (stored != null) {
+			AccessTokenPair accessToken = new AccessTokenPair(stored[0], stored[1]);
+			session = new AndroidAuthSession(appKeyPair, ACCESS_TYPE, accessToken);
+		} else {
+			session = new AndroidAuthSession(appKeyPair, ACCESS_TYPE);
+		}
+
+		return session;
+	}
+
+	/**
+	 * check sdcard is read only
+	 * 
+	 * @return
+	 */
+	public boolean isExternalStorageReadOnly() {
+		String extStorageState = Environment.getExternalStorageState();
+		if (Environment.MEDIA_MOUNTED_READ_ONLY.equals(extStorageState)) {
+			return true;
+		}
+		return false;
+	}
+
+	/**
+	 * check sdcard is available or not
+	 * 
+	 * @return
+	 */
+	public boolean isExternalStorageAvailable() {
+		String extStorageState = Environment.getExternalStorageState();
+		if (Environment.MEDIA_MOUNTED.equals(extStorageState)) {
+			return true;
+		}
+		return false;
+	}
+}
