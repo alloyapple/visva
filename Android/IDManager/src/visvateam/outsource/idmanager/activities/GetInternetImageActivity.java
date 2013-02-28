eb26b510fe528300291f9ef446c52d2e354f03b1
khaidt.hut <khaidt.hut@gmail.com>
2013-02-28 02:54:22 +0000

    finish follow edit

--- Android/IDManager/src/visvateam/outsource/idmanager/activities/GetInternetImageActivity.java
+++ Android/IDManager/src/visvateam/outsource/idmanager/activities/GetInternetImageActivity.java
@@ -3,35 +3,18 @@ package visvateam.outsource.idmanager.activities;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 
-public class SettingURLActivity extends Activity {
+public class GetInternetImageActivity extends Activity{
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.page_url_setup);
+		setContentView(R.layout.page_get_icon_of_internet);
 	}
-
 	public static void startActivity(Activity activity) {
-		Intent i = new Intent(activity, SettingURLActivity.class);
+		Intent i = new Intent(activity, GetInternetImageActivity.class);
 		activity.startActivity(i);
-	}
-
-	public void onReturn(View v) {
-		finish();
-	}
-
-	public void onRefresh(View v) {
-	}
-
-	public void onBroutherBack(View v) {
-
-	}
-
-	public void onBroutherNext(View v) {
-
 	}
 
 }
