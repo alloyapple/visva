eba2976e8dbac79720e967be7d64ce21dd2d777d
tranduc.bk.1905@gmail.com <tranduc.bk.1905@gmail.com@93bdda27-e0ef-8ee2-a4e7-4542826b0fda>
2013-05-03 02:46:39 +0000

    ID commit.

--- trunk/IOS/IDManager/IDManager/main.m
+++ trunk/IOS/IDManager/IDManager/main.m
@@ -13,6 +13,6 @@
 int main(int argc, char *argv[])
 {
     @autoreleasepool {
-        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TDAppDelegate class]));
+        return UIApplicationMain(argc, argv, NSStringFromClass([TDApplicationIdle class]), NSStringFromClass([TDAppDelegate class]));
     }
 }
