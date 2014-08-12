c473fec7bb4c703df279de97c8bcd47a4f050d81
kieuducthang <kieuducthang@gmail.com>
2014-08-12 07:19:09 +0000

    update some fragments

--- Android/Bravo/Bravo/src/com/visva/app/bravo/control/activity/ActivityLogin_Register.java
+++ Android/Bravo/Bravo/src/com/visva/app/bravo/control/activity/ActivityLogin_Register.java
@@ -1,16 +1,134 @@
 package com.visva.app.bravo.control.activity;
 
-import android.app.Activity;
+import java.util.ArrayList;
+
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 
 import com.visva.app.bravo.R;
+import com.visva.app.bravo.view.fragment.FragmentBravoRegister;
+import com.visva.app.bravo.view.fragment.FragmentLogin;
+import com.visva.app.bravo.view.fragment.FragmentRegister;
+import com.visva.app.bravo.view.fragment.FragmentRegisterUserInfo;
 
-public class ActivityLogin_Register extends Activity {
+public class ActivityLogin_Register extends FragmentActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login_register);
+	// ======================Constant Define===============
+	private static final String FRAGMENT_BRAVO_REGISTER = "bravo_register";
+	private static final String FRAGMENT_LOGIN = "login";
+	private static final String FRAGMENT_REGISTER = "register";
+	private static final String FRIEND_REGISTER_USER_INFO = "register_user_info";
 
-    }
+	private static final int FRAGMENT_BRAVO_REGISTER_ID = 1001;
+	private static final int FRAGMENT_LOGIN_ID = 1002;
+	private static final int FRAGMENT_REGISTER_ID = 1003;
+	private static final int FRAGMENT_REGISTER_USER_INFO_ID = 1004;
+
+	// ======================Class Define==================
+	private FragmentManager mFmManager;
+	private FragmentTransaction mTransaction;
+	private FragmentBravoRegister mFragmentBravoRegister;
+	private FragmentLogin mFragmentLogin;
+	private FragmentRegister mFragmentRegister;
+	private FragmentRegisterUserInfo mFragmentRegisterUserInfo;
+
+	// ======================Variable Define===============
+	private ArrayList<String> backstack = new ArrayList<String>();
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.page_fragment_bravo_register);
+
+		/* intialize fragments */
+		//initializeFragments();
+
+	}
+
+	private void initializeFragments() {
+		mFmManager = getSupportFragmentManager();
+		mFragmentBravoRegister = (FragmentBravoRegister) mFmManager
+				.findFragmentById(R.id.fragment_bravo_register);
+		mFragmentLogin = (FragmentLogin) mFmManager
+				.findFragmentById(R.id.fragment_login);
+		mFragmentRegister = (FragmentRegister) mFmManager
+				.findFragmentById(R.id.fragment_register);
+		mFragmentRegisterUserInfo = (FragmentRegisterUserInfo) mFmManager
+				.findFragmentById(R.id.fragment_bravo_user_info);
+
+		mTransaction = hideFragment();
+		showFragment(FRAGMENT_LOGIN_ID);
+	}
+
+	private void showFragment(int fragment) {
+		switch (fragment) {
+
+		case FRAGMENT_BRAVO_REGISTER_ID:
+			mTransaction = hideFragment();
+			mTransaction.show(mFragmentBravoRegister);
+			addToSBackStack(FRAGMENT_BRAVO_REGISTER);
+			mTransaction.commit();
+			break;
+
+		case FRAGMENT_LOGIN_ID:
+			mTransaction = hideFragment();
+			mTransaction.show(mFragmentLogin);
+			addToSBackStack(FRAGMENT_LOGIN);
+			mTransaction.commit();
+			break;
+
+		case FRAGMENT_REGISTER_ID:
+			mTransaction = hideFragment();
+			mTransaction.show(mFragmentRegister);
+			addToSBackStack(FRAGMENT_REGISTER);
+			mTransaction.commit();
+			break;
+
+		case FRAGMENT_REGISTER_USER_INFO_ID:
+			mTransaction = hideFragment();
+			mTransaction.show(mFragmentRegisterUserInfo);
+			addToSBackStack(FRIEND_REGISTER_USER_INFO);
+			mTransaction.commit();
+			break;
+		default:
+			break;
+		}
+	}
+
+	public FragmentTransaction hideFragment() {
+		mTransaction = mFmManager.beginTransaction();
+		mTransaction.hide(mFragmentBravoRegister);
+		mTransaction.hide(mFragmentLogin);
+		mTransaction.hide(mFragmentRegister);
+		mTransaction.hide(mFragmentRegisterUserInfo);
+		return mTransaction;
+	}
+
+	public void addToSBackStack(String tag) {
+		int index = backstack.lastIndexOf(tag);
+		if (index == -1) {
+			backstack.add(tag);
+			return;
+		}
+		try {
+			if (!backstack.get(index - 1).equals(
+					backstack.get(backstack.size() - 1))) {
+				backstack.add(tag);
+				return;
+			}
+		} catch (IndexOutOfBoundsException e) {
+
+		}
+		try {
+			ArrayList<String> subStack = new ArrayList<String>(backstack);
+			for (int i = 0; i < subStack.size(); i++) {
+				if (i > index) {
+					backstack.remove(index);
+				}
+			}
+		} catch (IndexOutOfBoundsException e) {
+		}
+	}
 }
---
+++ Android/Bravo/Bravo/src/com/visva/app/bravo/view/fragment/FragmentBravoRegister.java
@@ -0,0 +1,28 @@
+package com.visva.app.bravo.view.fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.visva.app.bravo.R;
+
+public class FragmentBravoRegister extends FragmentBasic {
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		View root = (ViewGroup) inflater.inflate(
+				R.layout.page_fragment_bravo_register, null);
+		return root;
+	}
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	}
+
+	@Override
+	public void onResume() {
+		super.onResume();
+	}
+}
