3f19911d6db2c97762b73f5c52d87c5c3c3fdd01
kieuducthang <kieuducthang@gmail.com>
2014-08-13 23:56:33 +0000

    create fragment login

--- Android/Bravo/Bravo/src/com/sharebravo/bravo/utils/BravoConstant.java
+++ Android/Bravo/Bravo/src/com/sharebravo/bravo/utils/BravoConstant.java
@@ -2,7 +2,11 @@ package com.sharebravo.bravo.utils;
 
 public class BravoConstant {
     /* Login-Register */
-    public static final String ACCESS_TYPE        = "access_type";
-    public static final int    ACCESS_BY_LOGIN    = 1;
-    public static final int    ACCESS_BY_REGISTER = 2;
+    public static final String ACCESS_TYPE                    = "access_type";
+
+    /* fragment id */
+    public static final int    FRAGMENT_BRAVO_REGISTER_ID     = 1001;
+    public static final int    FRAGMENT_LOGIN_ID              = 1002;
+    public static final int    FRAGMENT_REGISTER_ID           = 1003;
+    public static final int    FRAGMENT_REGISTER_USER_INFO_ID = 1004;
 }
