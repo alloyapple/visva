b0bbaad5e9e94d1a99ae4932c5ec10ea6e34f78a
kieuducthang <kieuducthang@gmail.com>
2014-08-16 08:56:53 +0000

    twitter login update

--- Android/Bravo/Bravo/src/com/sharebravo/bravo/utils/BravoConstant.java
+++ Android/Bravo/Bravo/src/com/sharebravo/bravo/utils/BravoConstant.java
@@ -42,16 +42,32 @@ public class BravoConstant {
     public static final int     FRAGMENT_BRAVO_LOGIN_ID            = FRAGMENT_REGISTER_USER_INFO_ID + 1;
     public static final int     FRAGMENT_FORGOT_PASSWORD           = FRAGMENT_BRAVO_LOGIN_ID + 1;
 
+    /* Twitter key and api */
     public static final String  BRAVO_PREFERENCE                   = "BRAVO_PREFERENCE";
-
-    public final static String  TWITTER_CONSUMER_KEY               = "Y1dwG6A8MBiy7nbhCPBi1w";
-
-    public final static String  TWITTER_CONSUMER_SECRET            = "9eNSdHTfP9eBoCcifoEWU4IrsUrvdqciaA4UgMoUK4";
-
-    public static final String  TWITTER_CALLBACK_URL               = "oauth://t4jsample";
+    public final static String  TWITTER_CONSUMER_KEY               = "hmouxtO1RpTRj8zXI0REdlesH";
+    public final static String  TWITTER_CONSUMER_SECRET            = "cPsBinH5CyWQBUpFcXZRz2QkXoeeec94erGXBdHrpl7TZmlwch";
+    public static final String  TWITTER_CALLBACK_URL               = "bravo://connect";
 
     // Twitter oauth urls
     public static final String  URL_TWITTER_AUTH                   = "auth_url";
     public static final String  URL_TWITTER_OAUTH_VERIFIER         = "oauth_verifier";
     public static final String  URL_TWITTER_OAUTH_TOKEN            = "oauth_token";
+
+    /* share preferences key */
+    public static final String  PREF_KEY_TWITTER_OAUTH_TOKEN       = "pref_key_twitter_oauth_token";
+    public static final String  PREF_KEY_TWITTER_OAUTH_VERIFIER    = "pref_key_twitter_oauth_verifier";
+
+    /* SNS Login, Register */
+    public static final String  LOGIN_SNS_TYPE                     = "login_sns_type";
+    public static final String  REGISTER_SNS_TYPE                  = "register_sns_type";
+    public static final int     NO_LOGIN_SNS                       = -1;
+    public static final int     LOGIN_FACEBOOK                     = NO_LOGIN_SNS + 1;
+    public static final int     LOGIN_TWITTER                      = LOGIN_FACEBOOK + 1;
+    public static final int     LOGIN_4SQUARE                      = LOGIN_TWITTER + 1;
+    public static final int     LOGIN_BRAVO                        = LOGIN_4SQUARE + 1;
+    public static final int     NO_REGISTER_SNS                    = LOGIN_BRAVO + 1;
+    public static final int     REGISTER_FACEBOOK                  = NO_REGISTER_SNS + 1;
+    public static final int     REGISTER_TWITTER                   = REGISTER_FACEBOOK + 1;
+    public static final int     REGISTER_4SQUARE                   = REGISTER_TWITTER + 1;
+    public static final int     REGISTER_BRAVO                     = REGISTER_4SQUARE + 1;
 }
--- Android/Bravo/Bravo/src/com/sharebravo/bravo/view/fragment/FragmentRegister.java
+++ Android/Bravo/Bravo/src/com/sharebravo/bravo/view/fragment/FragmentRegister.java
@@ -1,5 +1,6 @@
 package com.sharebravo.bravo.view.fragment;
 
+import twitter4j.RateLimitStatus;
 import twitter4j.Twitter;
 import twitter4j.TwitterException;
 import twitter4j.TwitterFactory;
@@ -7,17 +8,20 @@ import twitter4j.auth.AccessToken;
 import twitter4j.auth.RequestToken;
 import twitter4j.conf.Configuration;
 import twitter4j.conf.ConfigurationBuilder;
-import android.annotation.SuppressLint;
 import android.content.Intent;
-import android.net.Uri;
+import android.graphics.Bitmap;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Message;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+import br.com.condesales.EasyFoursquareAsync;
+import br.com.condesales.listeners.AccessTokenRequestListener;
+import br.com.condesales.listeners.ImageRequestListener;
+import br.com.condesales.listeners.UserInfoRequestListener;
+import br.com.condesales.models.User;
 
 import com.facebook.AppEventsLogger;
 import com.facebook.Session;
@@ -25,13 +29,15 @@ import com.facebook.SessionState;
 import com.facebook.UiLifecycleHelper;
 import com.facebook.model.GraphUser;
 import com.facebook.widget.LoginTextView;
+import com.sharebravo.bravo.MyApplication;
 import com.sharebravo.bravo.R;
 import com.sharebravo.bravo.control.activity.HomeActivity;
+import com.sharebravo.bravo.control.activity.WebAuthActivity;
 import com.sharebravo.bravo.model.user.BravoUser;
 import com.sharebravo.bravo.sdk.log.AIOLog;
 import com.sharebravo.bravo.utils.BravoConstant;
 
-public class FragmentRegister extends FragmentBasic {
+public class FragmentRegister extends FragmentBasic implements AccessTokenRequestListener, ImageRequestListener {
     // ====================Constant Define=================
     public static final int        TW_LOGIN_ERROR   = -1;
     public static final int        TW_LOGIN_SUCCESS = 0;
@@ -46,15 +52,24 @@ public class FragmentRegister extends FragmentBasic {
     private Session.StatusCallback mFacebookCallback;
     private static RequestToken    mTwitterRequestToken;
     protected static Twitter       mTwitter;
+    private EasyFoursquareAsync    mEasyFoursquareAsync;
+    private static int             mResgisterType;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View root = (ViewGroup) inflater.inflate(R.layout.page_fragment_register, container);
+        mResgisterType = BravoConstant.NO_REGISTER_SNS;
+        initializeView(root);
+        return root;
+    }
+
+    private void initializeView(View root) {
         mBtnBravoRegister = (Button) root.findViewById(R.id.btn_sign_up_bravo);
         mBtnBravoRegister.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
+                mResgisterType = BravoConstant.REGISTER_BRAVO;
                 mListener.showPageBravoRegister();
             }
         });
@@ -64,6 +79,7 @@ public class FragmentRegister extends FragmentBasic {
         mTextViewFacebookRegister.setUserInfoChangedCallback(new LoginTextView.UserInfoChangedCallback() {
             @Override
             public void onUserInfoFetched(GraphUser user) {
+                mResgisterType = BravoConstant.REGISTER_FACEBOOK;
                 AIOLog.e("user:" + user);
                 if (user == null)
                     return;
@@ -81,11 +97,7 @@ public class FragmentRegister extends FragmentBasic {
 
             @Override
             public void onClick(View v) {
-                // BravoUser bravoUser = new BravoUser();
-                // bravoUser.mUserEmail = "Kane";
-                // bravoUser.mUserId = "Kane";
-                // bravoUser.mUserName = "Kane";
-                // mListener.showPageUserInfo(bravoUser);
+                mResgisterType = BravoConstant.REGISTER_TWITTER;
                 onClickTwitterLoginButton();
             }
         });
@@ -96,15 +108,10 @@ public class FragmentRegister extends FragmentBasic {
 
             @Override
             public void onClick(View v) {
-                BravoUser bravoUser = new BravoUser();
-                bravoUser.mUserEmail = "Kane";
-                bravoUser.mUserId = "Kane";
-                bravoUser.mUserName = "Kane";
-                mListener.showPageUserInfo(bravoUser);
+                mResgisterType = BravoConstant.REGISTER_4SQUARE;
+                onClickLogin4Square();
             }
         });
-
-        return root;
     }
 
     @Override
@@ -136,66 +143,60 @@ public class FragmentRegister extends FragmentBasic {
         Session session = Session.getActiveSession();
         boolean isActivedSession = session.isOpened();
         AIOLog.d("isActivedSession=" + isActivedSession);
-        // Call the 'activateApp' method to log an app event for use in
-        // analytics and advertising reporting. Do so in
-        // the onResume methods of the primary Activities that an app may be
-        // launched into.
         AppEventsLogger.activateApp(getActivity());
-        // if (isTwitterLoggedInAlready()) {
-        // mTwitterLoginButton.setBackgroundResource(R.drawable.twitter_login_button_inactive);
-        // } else {
-        // mTwitterLoginButton.setBackgroundResource(R.drawable.twitter_login_button);
-        // }
 
-        // Twitter Login
-        // Get call back from Twitter side
-        Uri uri = getActivity().getIntent().getData();
-        if (uri != null && uri.toString().startsWith(BravoConstant.TWITTER_CALLBACK_URL)) {
-            // oAuth verifier
-            final String verifier = uri.getQueryParameter(BravoConstant.URL_TWITTER_OAUTH_VERIFIER);
-            AIOLog.d("verifier = " + verifier);
+        AIOLog.d("isTwitterLogined:" + mResgisterType);
+        if (isTwitterLoggedInAlready()) {
+            AIOLog.d("isTwitterLogined");
+            final String verifier = MyApplication.getInstance().getBravoSharePrefs().getStringValue(BravoConstant.PREF_KEY_TWITTER_OAUTH_VERIFIER);
+            // if()
+            
             Thread thread = new Thread(new Runnable() {
 
                 @Override
                 public void run() {
-                    int loginResult = TW_LOGIN_ERROR;
                     try {
+                        if (mTwitter == null || mTwitter.getRateLimitStatus("application") == null) {
+                            AIOLog.d("mTwitter is null");
+                            return;
+                        }
                         AccessToken accessToken = mTwitter.getOAuthAccessToken(mTwitterRequestToken, verifier);
-                        if (accessToken != null)
-                        {
+                        if (accessToken != null) {
                             // Log in successfully
-                            loginResult = TW_LOGIN_SUCCESS;
-                            // AlwaysSocialAppImpl.getInstance().getAlwaysSharePrefs().putBooleanValue(TwitterConstant.PREF_KEY_TWITTER_LOGIN, true);
-                            // AlwaysSocialAppImpl.getInstance().getAlwaysSharePrefs().putStringValue(TwitterConstant.PREF_KEY_TWITTER_OAUTH_TOKEN, accessToken.getToken());
-                            // AlwaysSocialAppImpl.getInstance().getAlwaysSharePrefs().putStringValue(TwitterConstant.PREF_KEY_TWITTER_OAUTH_SECRET, accessToken.getTokenSecret());
-                            // RateLimitStatus rateLimit = mTwitter.getRateLimitStatus("application").get("/application/rate_limit_status");
-                            // Log.d(TAG, "Application Remaining RateLimit =" + rateLimit.getRemaining());
-                            // int limitRate = rateLimit.getRemaining();
-                            // if (limitRate >= 20) {
-                            // long userID = accessToken.getUserId();
-                            // Log.d(TAG, "userID = " + userID);
-                            // twitter4j.User user = mTwitter.showUser(userID);
-                            // Log.d(TAG, "Twitter OAuth Token = " + accessToken.getToken() + " username = " + user.getName());
-                            // SocialUserObject loggedInUser = new SocialUserObject(Long.toString(user.getId()), user.getName(), user.getBiggerProfileImageURL());
-                            // AlwaysSocialAppImpl.getInstance().setLoggedInUser(GlobalConstant.SOCIAL_TYPE_TWITTER, loggedInUser);
-                            // }
+                            RateLimitStatus rateLimit = mTwitter.getRateLimitStatus("application").get("/application/rate_limit_status");
+                            if(rateLimit == null){
+                                AIOLog.d("rateLimit is null");
+                            }
+                            AIOLog.d("Application Remaining RateLimit =" + rateLimit.getRemaining());
+                            int limitRate = rateLimit.getRemaining();
+                            if (limitRate >= 10) { 
+                                long userID = accessToken.getUserId();
+                                AIOLog.d("userID = " + userID);
+                                twitter4j.User user = mTwitter.showUser(userID);
+                                AIOLog.d("Twitter OAuth Token = " + accessToken.getToken() + " username = " + user.getName());
+                                if (user != null) {
+                                    BravoUser _bravoUser = new BravoUser();
+                                    _bravoUser.mUserEmail = user.getName();
+                                    _bravoUser.mUserName = user.getName();
+                                    _bravoUser.mUserId = user.getId() + "";
+                                    mListener.showPageUserInfo(_bravoUser);
+                                }
+                            }
                         }
                     }
                     catch (TwitterException e) {
                         AIOLog.d("Error Twitter OAuth Token = " + e.getMessage());
-                        loginResult = TW_LOGIN_ERROR;
                     }
-                    mHandler.sendEmptyMessage(loginResult);
                 }
             });
-
             thread.start();
-            // mTwitterLoginButton.setEnabled(false);
-            // mViewType = AlwaysSocialAppImpl.getInstance().getViewType();
         }
     }
 
-    public void onClickTwitterLoginButton() {
+    /**
+     * twitter login and authen
+     */
+    private void onClickTwitterLoginButton() {
         // if not -> come to log in function
         ConfigurationBuilder builder = new ConfigurationBuilder();
         builder.setOAuthConsumerKey(BravoConstant.TWITTER_CONSUMER_KEY);
@@ -212,10 +213,12 @@ public class FragmentRegister extends FragmentBasic {
                 public void run() {
                     try {
                         mTwitterRequestToken = mTwitter.getOAuthRequestToken(BravoConstant.TWITTER_CALLBACK_URL);
-                        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(mTwitterRequestToken.getAuthenticationURL()));
+                        if (getActivity() == null)
+                            return;
+                        Intent intent = new Intent(getActivity(), WebAuthActivity.class);
                         intent.setFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
-                        getActivity().startActivity(intent);
-                        getActivity().finish();
+                        intent.putExtra("URL", mTwitterRequestToken.getAuthenticationURL());
+                        getActivity().startActivityForResult(intent, 0);
                     } catch (TwitterException e) {
                         e.printStackTrace();
                     }
@@ -229,29 +232,63 @@ public class FragmentRegister extends FragmentBasic {
     }
 
     private boolean isTwitterLoggedInAlready() {
+        int _loginType = MyApplication.getInstance().getBravoSharePrefs().getIntValue(BravoConstant.REGISTER_SNS_TYPE);
+        if (_loginType == mResgisterType)
+            return true;
         return false;
     }
 
-    @SuppressLint("HandlerLeak")
-    public final Handler mHandler = new Handler() {
+    /**
+     * foursquare api
+     */
+    @Override
+    public void onError(String errorMsg) {
+        // TODO Auto-generated method stub
 
-                                      @Override
-                                      public void handleMessage(Message msg) {
-                                          AIOLog.d("handleMessage msg.what = " + msg.what);
-                                          switch (msg.what) {
-                                          case TW_LOGIN_ERROR:
-                                              // mTwitterLoginButton.setEnabled(true);
-                                              // mTwitterLoginButton.setBackgroundResource(R.drawable.twitter_login_button);
-                                              break;
-                                          case TW_LOGIN_SUCCESS:
-                                              // mTwitterLoginButton.setEnabled(true);
-                                              // mTwitterLoginButton.setBackgroundResource(R.drawable.twitter_login_button_inactive);
-                                              break;
-                                          default:
-                                              break;
-                                          }
-                                          super.handleMessage(msg);
-                                      }
+    }
 
-                                  };
+    @Override
+    public void onImageFetched(Bitmap bmp) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void onAccessGrant(String accessToken) {
+        AIOLog.d("accessToken: " + accessToken);
+        // with the access token you can perform any request to foursquare.
+        mEasyFoursquareAsync.getUserInfo(new UserInfoRequestListener() {
+
+            @Override
+            public void onError(String errorMsg) {
+                // Some error getting user info
+                Toast.makeText(getActivity(), errorMsg, Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onUserInfoFetched(User user) {
+                // OWww. did i already got user!?
+                // if (user.getBitmapPhoto() == null) {
+                // UserImageRequest request = new UserImageRequest(getActivity(), getActivity());
+                // request.execute(user.getPhoto());
+                // } else {
+                // userImage.setImageBitmap(user.getBitmapPhoto());
+                AIOLog.d("user 4square:" + user);
+                if (user == null)
+                    return;
+                Toast.makeText(getActivity(), "Hello " + user.getFirstName() + " " + user.getLastName(), Toast.LENGTH_SHORT).show();
+                AIOLog.d("user: " + user.getFirstName());
+                BravoUser bravoUser = new BravoUser();
+                bravoUser.mUserEmail = user.getFirstName();
+                bravoUser.mUserId = String.valueOf(user.getId());
+                bravoUser.mUserName = user.getFirstName() + " " + user.getLastName();
+                mListener.showPageUserInfo(bravoUser);
+            }
+        });
+    }
+
+    private void onClickLogin4Square() {
+        mEasyFoursquareAsync = new EasyFoursquareAsync(getActivity());
+        mEasyFoursquareAsync.requestAccess(this);
+    }
 }
