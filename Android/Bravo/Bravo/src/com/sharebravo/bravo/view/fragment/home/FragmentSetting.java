ea217a2f5ca881324efbef545c5752b1ff8ed291
kieuducthang <kieuducthang@gmail.com>
2014-09-06 05:30:25 +0000

    add term of use

--- Android/Bravo/Bravo/src/com/sharebravo/bravo/view/fragment/home/FragmentSetting.java
+++ Android/Bravo/Bravo/src/com/sharebravo/bravo/view/fragment/home/FragmentSetting.java
@@ -1,14 +1,21 @@
 package com.sharebravo.bravo.view.fragment.home;
 
-import com.sharebravo.bravo.R;
-import com.sharebravo.bravo.view.fragment.FragmentBasic;
-
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
-public class FragmentSetting extends FragmentBasic{
+import com.sharebravo.bravo.R;
+import com.sharebravo.bravo.view.fragment.FragmentBasic;
+
+public class FragmentSetting extends FragmentBasic {
+    // =======================Constant Define==============
+    // =======================Class Define=================
+    private IShowPageTermOfUse iShowPageTermOfUse;
+    // =======================Variable Define==============
+    private TextView           mTextTermOfUse;
+    private TextView           mTextUpdateUserInfo;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -19,6 +26,31 @@ public class FragmentSetting extends FragmentBasic{
     }
 
     private void initializeView(View root) {
-        
+        mTextTermOfUse = (TextView) root.findViewById(R.id.text_term_of_use);
+        mTextUpdateUserInfo = (TextView) root.findViewById(R.id.text_edit_profile);
+
+        mTextUpdateUserInfo.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                showToast("This feature is comming soon");
+            }
+        });
+        mTextTermOfUse.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                iShowPageTermOfUse.showPageTermOfUse();
+            }
+        });
     }
+
+    public interface IShowPageTermOfUse {
+        public void showPageTermOfUse();
+    }
+
+    public void setListener(IShowPageTermOfUse iShowPageTermOfUse) {
+        this.iShowPageTermOfUse = iShowPageTermOfUse;
+    }
+
 }
---
+++ Android/Bravo/Bravo/src/com/sharebravo/bravo/view/fragment/home/FragmentTermOfUse.java
@@ -0,0 +1,53 @@
+package com.sharebravo.bravo.view.fragment.home;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import com.sharebravo.bravo.R;
+import com.sharebravo.bravo.view.fragment.FragmentBasic;
+
+@SuppressLint("SetJavaScriptEnabled")
+public class FragmentTermOfUse extends FragmentBasic {
+    // =============================Constant Define=====================
+    // ============================Control Define =====================
+    private WebView mWebView;
+
+    // ============================Class Define =======================
+
+    // ============================Variable Define =====================
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState) {
+        View root = (ViewGroup) inflater.inflate(R.layout.page_fragment_term_of_use, null);
+        mWebView = (WebView) root.findViewById(R.id.web_support);
+        mWebView.loadUrl("https://www.google.co.uk/");
+        WebSettings webSettings = mWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        return root;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    public class MyWebViewClient extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl("https://www.google.co.uk/");
+            return true;
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+}
