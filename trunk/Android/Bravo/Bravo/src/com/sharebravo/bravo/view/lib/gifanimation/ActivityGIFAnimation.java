2df812524e3484c2ccdc63bd1c140b7762dae33d
kieuducthang@gmail.com <kieuducthang@gmail.com@93bdda27-e0ef-8ee2-a4e7-4542826b0fda>
2014-09-22 11:09:54 +0000

    update bravo tab

--- trunk/Android/Bravo/Bravo/src/com/sharebravo/bravo/view/lib/gifanimation/ActivityGIFAnimation.java
+++ trunk/Android/Bravo/Bravo/src/com/sharebravo/bravo/view/lib/gifanimation/ActivityGIFAnimation.java
@@ -1,28 +1,37 @@
-package com.sharebravo.bravo.control.activity;
+package com.sharebravo.bravo.view.lib.gifanimation;
 
 import java.io.InputStream;
 
-import android.app.Activity;
+import android.annotation.SuppressLint;
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Canvas;
 import android.graphics.Movie;
+import android.graphics.Paint;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup.LayoutParams;
 
 import com.sharebravo.bravo.R;
+import com.sharebravo.bravo.sdk.log.AIOLog;
 
-public class ActivityGIFAnimation extends Activity {
+@SuppressLint("DrawAllocation")
+public class ActivityGIFAnimation extends GraphicsActivity {
     private static final long TIME_TO_FINISH = 14500;
     private Movie             mMovie;
     private InputStream       mInputStream   = null;
     private long              mMovieStart;
+    private int               mWidth, mHeight;
 
+    @SuppressWarnings("deprecation")
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(new GIFView(this));
-        // setContentView(R.layout.activity_splash);
+        View view = new GIFView(this);
+        mWidth = getWindowManager().getDefaultDisplay().getWidth();
+        mHeight = getWindowManager().getDefaultDisplay().getHeight();
+        view.setLayoutParams(new LayoutParams(mWidth, mHeight));
+        setContentView(view);
 
         mInputStream = getResources().openRawResource(R.drawable.bravo_jump);
         mMovie = Movie.decodeStream(mInputStream);
@@ -39,17 +48,21 @@ public class ActivityGIFAnimation extends Activity {
         @Override
         protected void onDraw(Canvas canvas) {
             super.onDraw(canvas);
+            Paint paint = new Paint();
+            paint.setAntiAlias(true);
             long now = android.os.SystemClock.uptimeMillis();
             if (mMovieStart == 0) {
                 mMovieStart = now;
             }
             int relTime = (int) ((now - mMovieStart) % mMovie.duration());
-            Log.d("KieuThang", "relTime1:" + relTime + ", movie1.duration():" + mMovie.duration() + ", moviestart1:" + mMovieStart);
             if (relTime > TIME_TO_FINISH) {
+                Intent intent = new Intent();
+                setResult(RESULT_OK, intent);
                 finish();
             }
             mMovie.setTime(relTime);
-            mMovie.draw(canvas, 0, 80);
+            AIOLog.d("movie width:" + mMovie.width() + ", movie height:" + mMovie.height());
+            mMovie.draw(canvas, 0, 80, paint);
             this.invalidate();
         }
     }
---
+++ trunk/Android/Bravo/Bravo/src/com/sharebravo/bravo/view/lib/gifanimation/GraphicsActivity.java
@@ -0,0 +1,28 @@
+package com.sharebravo.bravo.view.lib.gifanimation;
+
+
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+
+class GraphicsActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void setContentView(View view) {
+        if (false) { // set to true to test Picture
+            ViewGroup vg = new PictureLayout(this);
+            vg.addView(view);
+            view = vg;
+        }
+        
+        super.setContentView(view);
+    }
+}
+
